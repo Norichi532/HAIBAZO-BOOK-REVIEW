5d273849fffb40c5aa0bee725128cf2a33be2905
Tan Phat <impphatdn99@gmail.com>
2026-06-03 11:48:13 +0700

    Create request schemas

---
+++ backend/schemas.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+
+class AuthorBase(BaseModel):
+    name: str
+
+class AuthorCreate(AuthorBase):
+    pass
+
+class AuthorResponse(AuthorBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class BookBase(BaseModel):
+    title: str
+    author_id: int
+
+class BookCreate(BookBase):
+    pass
+
+class BookResponse(BookBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class ReviewBase(BaseModel):
+    review: str
+    book_id: int
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class ReviewResponse(ReviewBase):
+    id: int
+
+    class Config:
+        from_attributes = True  
